04fcd7eb68e1e5a0cd23620ea9016c74b95fa835
Bharath <njbharathkumar@gmail.com>
2018-02-08 14:46:55 +0530

    initial commiv -v 1.0

---
+++ js/init.js
@@ -0,0 +1,29 @@
+(function($){
+  $(function(){
+    $('.collapsible').collapsible();
+    $('.parallax').parallax();
+    $('.modal').modal({
+              dismissible: false, // Modal can be dismissed by clicking outside of the modal
+              opacity: .5, // Opacity of modal background
+              inDuration: 300, // Transition in duration
+              outDuration: 200, // Transition out duration
+              startingTop: '4%', // Starting top style attribute
+              endingTop: '10%', // Ending top style attribute
+              ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+              },
+              complete: function() {
+               } // Callback for Modal close
+            }
+    );
+
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
+    }
+  );
+  }); 
+})(jQuery); // end of jQuery name space
